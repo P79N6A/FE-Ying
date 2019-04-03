8217698c571f818b08bb7cfdfc0c60bc1c85a617
郝栩彬 <haoxubin@bytedance.com>
2019-04-03 10:49:20 +0800

    feat: 创建plugin loader工程

--- feying-loader/gulpfile.js
+++ feying-loader/gulpfile.js
@@ -1,11 +1,33 @@
 const gulp = require('gulp');
 const ts = require('gulp-typescript');
-
+const { watch } = require('gulp');
 const tsProject = ts.createProject('tsconfig.json');
+const watcher = watch(['./src/**/*.ts']);
 
-exports.default = function(cb) {
-    tsProject.src()
-    .pipe(tsProject())
-    .pipe(gulp.dest('dist'))
+const staticPath = ['./src/**/*.txt', './src/**/*.json', './src/**/*.js'];
+
+const staticTask = cb => {
+    gulp.src(staticPath).pipe(gulp.dest('dist'));
     cb();
-}
+};
+
+const build = cb => {
+    // staticTask(cb)
+    tsProject
+        .src()
+        .pipe(tsProject())
+        .pipe(gulp.dest('dist'));
+    cb();
+};
+
+const watchTask = cb => {
+    build(cb);
+    watcher.on('change', () => {
+        build(cb);
+        cb();
+    });
+};
+
+exports.build = build;
+
+exports.watch = watchTask;
